d4e40d69ad5b0d44dd044bf7438446d26255ff6b
hyperscaledesignhub <thefreedomcodex@gmail.com>
2026-02-21 09:52:47 +0530

    adding combinations

--- Recursion/practise-2/06-combinations.py
+++ Recursion/practise-2/06-combinations.py
@@ -1,15 +1,20 @@
 def combinations(nums:list[str],comb_size:int):
     result=[]
+    n=len(nums)
     def helper(slate:list[str],i:int):
         #Base case
-        if i == comb_size:
+        if len(slate) == comb_size:
             result.append(slate[:])
             return
-        #Recursive case
-        for k in range(i,len(nums)):
-            slate.append(nums[k])
-            helper(slate,i+1)
-            slate.pop()
+        if i == n:
+            return
+            
+        #Exclude case
+        helper(slate,i+1)
+        #Include case
+        slate.append(nums[i])
+        helper(slate,i+1)
+        slate.pop()
     helper([],0)
     return result
 input=['A','B','C','D']
